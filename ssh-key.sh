aecd01c2e2bd659bcf92a0fbf71781caab37a554
Technical Heist <34364969+technicalheist@users.noreply.github.com>
2024-12-09 11:04:30 +0530

    Update ssh-key.sh

--- ssh-key.sh
+++ ssh-key.sh
@@ -2,8 +2,10 @@
 
 # Prompt user for email ID
 read -p "Enter your email ID: " email
+read -p "Enter SSH Key Name " name
 
 # Generate SSH key with provided email
-ssh-keygen -t rsa -b 4096 -C "$email"
+ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/$name
+
 
 echo "SSH key generated successfully!"
